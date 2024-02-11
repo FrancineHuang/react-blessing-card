86cfcedd42121cda4bb20106e6476b0811e1fe21
FrancineHuang <huangxin130@gmail.com>
2024-02-11 21:18:20 +0900

    test: 最初の動作確認

--- react-blessing-card/src/App.tsx
+++ react-blessing-card/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          The development of the blessing card is under construction
         </p>
         <a
           className="App-link"
