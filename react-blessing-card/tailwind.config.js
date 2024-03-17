977891fb29f12ff9cd25fea61b847fcf8f3ee2d2
FrancineHuang <huangxin130@gmail.com>
2024-03-17 18:15:04 +0900

    chore: primary setting for the tailwind css and shadcn ui

--- react-blessing-card/tailwind.config.js
+++ react-blessing-card/tailwind.config.js
@@ -1,68 +1,69 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const { fontFamily } = require("tailwindcss/defaultTheme");
+const { fontFamily } = require('tailwindcss/defaultTheme');
 
-const customColors = require("./config/tailwind/colors");
+const customColors = require('./config/tailwind/colors');
 
 /** @type {import('tailwindcss').Config} */
 (
   module.exports = {
-    // tailwindcssのスタイルを適用するファイルを指定(含めてないと特殊なスタイルが効かない)
+    // temperately configure the template paths
     content: [
-      "src/*.{ts.tsx}",
-      "components/*.{ts,tsx}",
-      "pages/*.{ts,tsx}",
-      "hooks/*.{ts,tsx}",
-      "utils/*.{ts,tsx}",
+      'src/*.{ts.tsx}',
+      'components/*.{ts,tsx}',
+      'pages/*.{ts,tsx}',
+      'hooks/*.{ts,tsx}',
+      'utils/*.{ts,tsx}',
     ],
     theme: {
       extend: {
-        // フォント設定
+        // font settings
         fontFamily: {
-          sans: ["var(--font-sans)", ...fontFamily.sans],
+          sans: ['var(--font-sans)', ...fontFamily.sans],
         },
 
-        // カラー設定
+        // color settings
         ...customColors,
 
-        // shadcn用設定
+        // settings for shadcn
         keyframes: {
-          "accordion-down": {
+          'accordion-down': {
             from: { height: 0 },
-            to: { height: "var(--radix-accordion-content-height)" },
+            to: { height: 'var(--radix-accordion-content-height)' },
           },
-          "accordion-up": {
-            from: { height: "var(--radix-accordion-content-height)" },
+          'accordion-up': {
+            from: { height: 'var(--radix-accordion-content-height)' },
             to: { height: 0 },
           },
         },
         animation: {
-          "accordion-down": "accordion-down 0.2s ease-out",
-          "accordion-up": "accordion-up 0.2s ease-out",
+          'accordion-down': 'accordion-down 0.2s ease-out',
+          'accordion-up': 'accordion-up 0.2s ease-out',
         },
 
-        // ブレークポイントカスタマイズ設定(デフォルトのsmなどはそのまま使える)
-        // 何も指定しない場合がモバイル用のスタイルとなる
+        // customize the breakpoint settings(those default value like 'sm' still can be used)
+        // if there is no any description, the default value will be used in the mobile display
         screens: {
-          tablet: "640px",
+          tablet: '640px',
           // => @media (min-width: 640px) { ... } sm
 
-          laptop: "1024px",
+          laptop: '1024px',
           // => @media (min-width: 1024px) { ... } lg
 
-          "sm-desktop": "1120px",
+          'sm-desktop': '1120px',
           // => @media (min-width: 1120px) { ... } sm-desktop
 
-          desktop: "1280px",
+          desktop: '1280px',
           // => @media (min-width: 1280px) { ... } xl
 
-          "3xl": "1800px",
+          '3xl': '1800px',
           // => @media (min-width: 1800px) { ... } 3xl
 
-          "4xl": "2000px",
+          '4xl': '2000px',
           // => @media (min-width: 2100px) { ... } 4xl
         },
       },
     },
-    plugins: [require("tailwindcss-animate")],
+    plugins: [require('tailwindcss-animate')],
   }
 );
+
