b52722fd262ec414b6248a1e9f8da39d33b8106e
FrancineHuang <huangxin130@gmail.com>
2024-03-20 22:34:08 +0900

    chore: migrate to the next.js

---
+++ react-blessing-card/src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from 'next'
+
+// These styles apply to every route in the application
+import './global.css'
+
+export const metadata: Metadata = {
+  title: 'React App',
+  description: 'Web site created with Next.js.',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <div id="root">{children}</div>
+      </body>
+    </html>
+  )
+}
