eb3930c2055af57c457b9669fd2f99b35c4763dc
FrancineHuang <huangxin130@gmail.com>
2024-03-21 22:44:43 +0900

    chore: change the display from app to index

---
+++ react-blessing-card/src/components/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "../App.css";
+
+export default function Index() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>The development of the blessing card is under construction</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
